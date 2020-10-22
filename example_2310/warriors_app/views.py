2fe088ccf738ea4a86351c5b0e1cc57aa7da1273
TonikX <antongovorov@gmail.com>
2020-10-23 00:01:28 +0300

    new endpoints for  warrior

--- example_2310/warriors_app/views.py
+++ example_2310/warriors_app/views.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
 
 
 from .models import Warrior
@@ -13,5 +14,54 @@ class WarriorAPIView(APIView):
         warriors = Warrior.objects.all()
         serializer = WarriorSerializer(warriors, many=True)
         return Response({"Warriors": serializer.data})
+
+
+    def post(self, request):
+        serializer = WarriorSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            article_saved = serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def put(self, request, pk):
+        warrior = get_object_or_404(Warrior, pk=pk)
+        serializer = WarriorSerializer(warrior, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def delete(self, request, pk):
+        warrior = get_object_or_404(Warrior, pk=pk)
+        try:
+            warrior.delete()
+            return Response(status=200)
+        except:
+            return Response(status=400)
+
+
+class WarriorCreateAPIView(generics.CreateAPIView):
+    serializer_class = WarriorSerializer
+    queryset = Warrior.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class WarriorDestroyView(generics.DestroyAPIView):
+    queryset = Warrior.objects.all()
+    serializer_class = WarriorSerializer
+
+
+class WarriorUpdateView(generics.UpdateAPIView):
+    queryset = Warrior.objects.all()
+    serializer_class = WarriorSerializer
+
+
+class WarriorDetailsView(generics.RetrieveAPIView):
+    queryset = Warrior.objects.all()
+    serializer_class = WarriorSerializer
 
 
