22a64b4b2328047256589c0cb80fad820736a373
kantegory <davidobryakov@gmail.com>
2020-10-23 09:22:44 +0300

    Add example urls for views

--- example_2310/warriors_app/urls.py
+++ example_2310/warriors_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import WarriorAPIView
+from .views import *
 
 
 app_name = "warriors_app"
@@ -7,4 +7,10 @@ app_name = "warriors_app"
 
 urlpatterns = [
     path('warriors/', WarriorAPIView.as_view()),
+    path('warriors/list/', WarriorListAPIView.as_view()),
+    path('warriors/list/related/', WarriorListRelatedAPIView.as_view()),
+    path('warriors/list/depth/', WarriorListDepthAPIView.as_view()),
+    path('warriors/list/nested/', WarriorListNestedAPIView.as_view()),
+
+    path('profession/create/', ProfessionCreateView.as_view())
 ]
