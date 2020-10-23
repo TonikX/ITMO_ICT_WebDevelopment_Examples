8f90e43a704d0ab9304a3e8403fdc4fd57797e5d
kantegory <davidobryakov@gmail.com>
2020-10-23 09:22:24 +0300

    Add example views for serializers

--- example_2310/warriors_app/views.py
+++ example_2310/warriors_app/views.py
@@ -1,11 +1,12 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import generics
 
 
-from .models import Warrior
+from .models import *
 
 
-from .serializer import WarriorSerializer
+from .serializers import *
 
 
 class WarriorAPIView(APIView):
@@ -15,3 +16,35 @@ class WarriorAPIView(APIView):
         return Response({"Warriors": serializer.data})
 
 
+class ProfessionCreateView(APIView):
+	def post(self, request):
+		print("REQUEST DATA", request.data)
+		profession = request.data.get("profession")
+		print("PROF DATA", profession)
+
+		serializer = ProfessionCreateSerializer(data=profession)
+
+		if serializer.is_valid(raise_exception=True):
+			profession_saved = serializer.save()
+
+		return Response({"Success": "Profession '{}' created succesfully.".format(profession_saved.title)})
+
+
+class WarriorListAPIView(generics.ListAPIView):
+	queryset = Warrior.objects.all()
+	serializer_class = WarriorSerializer
+
+
+class WarriorListRelatedAPIView(generics.ListAPIView):
+	queryset = Warrior.objects.all()
+	serializer_class = WarriorRelatedSerializer
+
+
+class WarriorListDepthAPIView(generics.ListAPIView):
+	queryset = Warrior.objects.all()
+	serializer_class = WarriorDepthSerializer
+
+
+class WarriorListNestedAPIView(generics.ListAPIView):
+	queryset = Warrior.objects.all()
+	serializer_class = WarriorNestedSerializer
